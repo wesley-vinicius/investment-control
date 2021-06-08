3ae6c20feec45d25a1d1f4eebf89b5f23fd90fff
Wesley Vinicius <viweesleyy9923@gmail.com>
2021-06-08 20:15:59 -0300

    feat(user): criar usuario

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::prefix('auth')->name('auth.')->group(function () {
+    Route::post('register', [RegisterController::class, 'register'])->name('register');  
 });
