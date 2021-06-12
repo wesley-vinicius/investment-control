d7c829fe16923f07c491c7c3d417cd03dcffad40
Wesley Vinicius <viweesleyy9923@gmail.com>
2021-06-12 15:19:23 -0300

    test(auth): teste rota me

---
+++ tests/Feature/Domain/Auth/Http/Controller/MeTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature\Domain\Auth\Http\Controller;
+
+use App\Domain\Auth\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Sanctum\Sanctum;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
+
+class MeTest extends TestCase
+{
+    use DatabaseMigrations;
+    
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testLoggedOutUserNotHaveAccess()
+    {
+        $response = $this->getJson(route('auth.me'));
+
+        $response->assertStatus(Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function testLoggedInUserHasAccess()
+    {
+        $user = Sanctum::actingAs(
+            User::factory()->create()
+        );
+        
+        $response = $this->getJson(route('auth.me'));
+        $response->assertOk()
+        ->assertExactJson($user->toArray());
+    }
+}
