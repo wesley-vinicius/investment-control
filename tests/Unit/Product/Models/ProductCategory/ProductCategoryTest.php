54d52881a0fb2bea86ef5d43a1c545f5f1825968
Wesley Vinicius <viweesleyy9923@gmail.com>
2021-06-27 12:00:28 -0300

    feat(product): teste para o model ProductCategory

---
+++ tests/Unit/Product/Models/ProductCategory/ProductCategoryTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit\Product\Models\ProductCategory;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Schema;
+use Tests\TestCase;
+
+class ProductCategoryTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testUsersDatabaseHasExpectedColumns()
+    {
+        $this->assertTrue(
+            Schema::hasColumns('product_categories', [
+                'id', 'name'
+            ]),
+            1
+        );
+    }
+}
